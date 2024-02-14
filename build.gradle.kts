8b5309be0d57bfc8043207fcaeb4ddcb7d5586a6
Alden Luthfi <alden.luthfi@ui.ac.id>
2024-02-14 19:40:54 +0700

    Sonarcloud fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
-val seleniumJavaVersion = "4.14.1"
-val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion = "5.9.1"
-
-
 plugins {
 	java
 	jacoco
+	id("org.sonarqube") version "4.4.1.3373"
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
 }
@@ -28,6 +23,11 @@ repositories {
 	mavenCentral()
 }
 
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdriverManagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
@@ -36,24 +36,26 @@ dependencies {
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
-	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
-	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+	testImplementation("org.seleniumhq.selenium:selenium-java:${seleniumJavaVersion}")
+	testImplementation("io.github.bonigarcia:selenium-jupiter:${seleniumJupiterVersion}")
+	testImplementation("io.github.bonigarcia:webdrivermanager:${webdriverManagerVersion}")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:${junitJupiterVersion}")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitJupiterVersion}")
 }
 
 tasks.register<Test>("unitTest") {
-	description = "Run unit tests"
+	description = "Runs unit tests."
 	group = "verification"
+
 	filter {
 		excludeTestsMatching("*FunctionalTest")
 	}
 }
 
 tasks.register<Test>("functionalTest") {
-	description = "Run functional tests"
+	description = "Runs functional tests."
 	group = "verification"
+
 	filter {
 		includeTestsMatching("*FunctionalTest")
 	}
@@ -67,9 +69,23 @@ tasks.test {
 	filter {
 		excludeTestsMatching("*FunctionalTest")
 	}
+
 	finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
 	dependsOn(tasks.test)
+
+	reports {
+		html.required = true
+		xml.required = true
+	}
+}
+
+sonar {
+	properties {
+		property("sonar.host.url", "https://sonarcloud.io")
+		property("sonar.organization", "aldenluthfi")
+		property("sonar.projectKey", "aldenluthfi_eshop")
+	}
 }
