284408a32c2fbaa684cbdf285a84bf83f1b2020f
Alden Luthfi <alden.luthfi@ui.ac.id>
2024-03-06 17:41:54 +0700

    [REFACTOR] Apply OrderStatus enum check to Order model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,11 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 
 @Builder @Getter
 public class Order {
@@ -15,11 +15,11 @@ public class Order {
     private String author;
     private String status;
 
-    public Order(String id, List<Product> products, Long orderTime, String author) {
+    public Order(String id, List<Product> products, Long orderTime, String author){
         this.id = id;
         this.orderTime = orderTime;
         this.author = author;
-        this.status = "WAITING_PAYMENT";
+        this.status= OrderStatus.WAITINGPAYMENT.getValue();
 
         if (products.isEmpty()){
             throw new IllegalArgumentException();
@@ -33,11 +33,10 @@ public class Order {
     }
 
     public void setStatus(String status){
-        String[] statusList = {"WAITING PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
-        if (Arrays.stream((statusList)).noneMatch(item -> (item.equals(status)))){
-            throw new IllegalArgumentException();
-        }else{
+        if (OrderStatus.contains(status)){
             this.status = status;
+        }else{
+            throw new IllegalArgumentException();
         }
     }
 }
